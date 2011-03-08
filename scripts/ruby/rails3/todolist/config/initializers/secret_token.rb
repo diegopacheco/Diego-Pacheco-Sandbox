d0b8c5723dfb7197c9fc9d148110ba9dfdd3985a
diegopacheco <diego.pacheco.it@gmail.com>
2011-03-08 05:03:57 -0300

    added first rails 3 app.

---
+++ scripts/ruby/rails3/todolist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = '7dfb7f15aa9aa6329af9ce16dc14aa792d8ead1bb100a28d1cddd474e3d765957e89fa0804858c4ac0223a3e8040073febc4fc2ce3248f8a8c96cf8cc9507bcb'
