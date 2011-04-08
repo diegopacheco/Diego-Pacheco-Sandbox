a54ccba744afa7d20465b5dc6ebae172889d480f
diegopacheco <diego.pacheco.it@gmail.com>
2011-04-08 19:05:32 -0300

    added very cool scala code bean2short.scala

---
+++ scripts/scala/scala-playground/src/bean2short.scala
@@ -0,0 +1,21 @@
+import scala.reflect._
+
+/**
+ * Class with constructor and more fun with Scala
+ * @author Diego Pacheco
+ *
+ */
+class BeanShort(
+	@BeanProperty var name:String = "Nothing",	
+  	@BeanProperty var steps:List[String] = List("ZERO"),
+  	@BeanProperty var args:Map[Int,String] = Map(1 -> "zero")
+){
+  override def toString() : String = "Name: " + getName() + ", Args: " + getArgs() + ",Steps: " + getSteps()	
+}
+
+object bean2short {
+  def main(args : Array[String]) : Unit = {
+	   var b =  new BeanShort( name = "Diego Pacheco" )
+       printf("Object result: %s",b)
+  }
+}
