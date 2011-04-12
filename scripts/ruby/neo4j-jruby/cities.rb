f5acd1ff7e0956b259eb9fb03470a540d2276bff
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-04-12 00:16:34 -0300

    added neo4j jruby code to fix cities issue

---
+++ scripts/ruby/neo4j-jruby/cities.rb
@@ -0,0 +1,48 @@
+require "rubygems"
+require "neo4j"
+require "java"
+
+class PathFinder
+	def print_shortest_path(from,to)		
+	    puts "route from #{from[:name]} to #{to[:name]}"
+		it   = Neo4j::Algo.shortest_path(from,to).iterator
+		last = from[:name]
+		it.next
+		while it.hasNext
+			n = it.next
+			if !n[:name].blank?
+				puts "#{last} -> #{n[:name]}"
+				last = n[:name]
+			end
+		end
+	end
+end
+
+Neo4j::Transaction.run do
+	
+	miami   = Neo4j::Node.new(:name => 'Miami'       , :state => "Florida"    , :ab => "FL")
+	ny      = Neo4j::Node.new(:name => 'New York'    , :state => "New York"   , :ab => "NY")
+	wichita = Neo4j::Node.new(:name => 'Wichita'     , :state => "Kansas"     , :ab => "KS")
+	austin  = Neo4j::Node.new(:name => 'Austin'      , :state => "Texas"      , :ab => "TX")
+	detroit = Neo4j::Node.new(:name => 'Detroit'     , :state => "Michigan"   , :ab => "MI")
+	denver  = Neo4j::Node.new(:name => 'Denver'      , :state => "Colorado"   , :ab => "CO")	
+	la      = Neo4j::Node.new(:name => 'Los Angeles' , :state => "California" , :ab => "CA")
+	
+	miami.outgoing(:to) << ny
+	miami.outgoing(:to) << austin
+	ny.outgoing(:to) << miami
+	ny.outgoing(:to) << detroit
+	detroit.outgoing(:to) << ny
+	detroit.outgoing(:to) << wichita
+	wichita.outgoing(:to) << detroit
+	wichita.outgoing(:to) << denver
+	denver.outgoing(:to) << wichita
+	denver.outgoing(:to) << la
+	austin.outgoing(:to) << miami
+	la.outgoing(:to) << austin
+	la.outgoing(:to) << denver
+	
+	pf = PathFinder.new	
+	pf.print_shortest_path(miami,wichita)
+
+end
