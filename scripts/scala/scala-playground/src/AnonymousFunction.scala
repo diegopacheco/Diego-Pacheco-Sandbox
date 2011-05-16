286c4dc10d2671e2c997a9e7e9a8eaf94db238f4
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-05-16 01:25:06 -0300

    added scala kick ass code

---
+++ scripts/scala/scala-playground/src/AnonymousFunction.scala
@@ -0,0 +1,14 @@
+object AnonymousFunction {
+  
+  def main(args : Array[String]) : Unit = {
+	  logCall( () => 
+      	Console.println("Chamada de metodo atual!")
+      )   
+  }
+  
+  def logCall(callback: () => Unit): Unit =  {
+	  Console.print("Log antes de chamar seu metodo... Invocado as : " + System.currentTimeMillis + "\n")
+	  callback()
+  }
+  
+}
---
+++ scripts/scala/scala-playground/src/ConsoleUsage.scala
@@ -0,0 +1,5 @@
+object ConsoleUsage {
+  def main(args : Array[String]) : Unit = {
+	  Console.println("Print out on console without pain!");
+  }
+}
