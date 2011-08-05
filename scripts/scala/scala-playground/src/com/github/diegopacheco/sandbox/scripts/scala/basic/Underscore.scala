69e9dd6a22be117fb07e4753288100e12d254be2
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-08-04 21:11:42 -0300

    added more scala code to play with scala core code lang features.

--- scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/basic/Underscore.scala
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/basic/Underscore.scala
@@ -2,10 +2,16 @@ package com.github.diegopacheco.sandbox.scripts.scala.basic
 
 object Underscore {
   
+  def soma = sum(_,_)
+  def somaTodos = sumAll(_)
+  
   def sum(a:Int,b:Int) =  sumAll(List(a,b))
-  def sumAll(is:List[Int]) = is.reduceLeft(_+_)
+  def sumAll(is:List[Int]) = is.reduceLeft(_+_) 
   
   def main(args : Array[String]) : Unit = {
-	  println( sum(4,3) )
+	  println( soma(4,3) )
+	  
+	  var l = List(1,2,3,4,5)
+	  println( somaTodos(l) )
   }
 }
