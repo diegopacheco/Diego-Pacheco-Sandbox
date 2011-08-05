3e33e1561c98462c4f9ec0f67da42643a1ebe86d
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-08-05 01:06:36 -0300

    added more scala core

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/packageobject/PackageObjectScala.scala
@@ -0,0 +1,3 @@
+//package object scala {
+//   def soma2i(a:Int,b:Int) = a + b
+//} 
---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/packageobject/TestPackageObject.scala
@@ -0,0 +1,7 @@
+package com.github.diegopacheco.sandbox.scripts.scala.packageobject
+
+object TestPackageObject {
+  def main(args : Array[String]) : Unit = {
+//    println( soma2i(2,3) )
+  }
+}
