243a67dd06df6e0aca783a2d6c8016bbd48a1148
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-08-28 22:13:38 -0300

    added scala test code

---
+++ scripts/scala/scala-test/ExampleSuite.scala
@@ -0,0 +1,10 @@
+import org.scalatest.FeatureSpec
+ 
+class ExampleSpec extends FeatureSpec {
+ 
+  feature("The user can pop an element off the top of the stack") { 
+    scenario("pop is invoked on a non-empty stack") (pending) 
+    scenario("pop is invoked on an empty stack") (pending)
+  }
+  
+}
