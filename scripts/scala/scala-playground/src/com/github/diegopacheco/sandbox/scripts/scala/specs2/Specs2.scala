ac1c62c74fe69d9dfa1769596c17bd06dfc64cfb
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-12-20 21:10:43 -0200

    fun with specs2 and scala embed runner.

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/specs2/Specs2.scala
@@ -0,0 +1,21 @@
+package com.github.diegopacheco.sandbox.scripts.scala.specs2
+
+import org.specs2.mutable._
+import org.specs2.runner.ClassRunner
+
+class ArithmeticSpec extends Specification {  
+   "The Aritmethic" should {
+      "add two numbers" in {
+    	  1 + 1 mustEqual 2
+      }
+      "add three numbers" in {
+    	  1 + 1 + 1 mustEqual 3
+      }
+    }
+}
+
+object MainSpec2RunA{
+   def main(args: Array[String]) {
+	   new org.specs2.runner.ClassRunner().apply(new ArithmeticSpec)
+   }
+}
