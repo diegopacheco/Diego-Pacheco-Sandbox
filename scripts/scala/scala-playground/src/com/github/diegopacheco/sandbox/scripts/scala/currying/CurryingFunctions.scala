67f14efdf11d06b62465912b92434a2136b59c97
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-12-27 13:05:19 -0200

    Fun with currying

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/currying/CurryingFunctions.scala
@@ -0,0 +1,12 @@
+package com.github.diegopacheco.sandbox.scripts.scala.currying
+
+object CurryingFunctions {
+	
+	def times(n:Int)(f: => Unit) = for(i <- 1 to n){ f }
+	
+	def main(args: Array[String]) {
+		times(5){ 
+		   println("Hey")
+		}
+	}
+}
