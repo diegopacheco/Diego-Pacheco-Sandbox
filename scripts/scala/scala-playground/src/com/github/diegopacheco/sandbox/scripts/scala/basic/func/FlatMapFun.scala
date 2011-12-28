960ba32bf56cfe05adde1db692c70f48c85ec5ca
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-12-28 17:47:27 -0200

    added some code with functional code

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/basic/func/FlatMapFun.scala
@@ -0,0 +1,31 @@
+package com.github.diegopacheco.sandbox.scripts.scala.basic.func
+
+object FlatMapFunMain {
+     
+    object Time{
+		def bench(m:String)(f: => Unit){
+			val init = System.currentTimeMillis()
+			f
+			val end = System.currentTimeMillis()
+			printf("{%s} execution in %d\n", m,(end - init))
+		}
+    }
+  
+	def main(args: Array[String]) {
+		
+	    import Time._
+	  
+		val names    = List("Diego","Andre","Alexandre","Jackson","Jose","Jeferson")
+		val surnames = List("on","dre")
+		
+		bench("flatmap"){
+	    	val result = names.par.flatMap { n =>
+			surnames.par 
+			        .filter(s => n endsWith s)
+			        .map(s => (n, s)) 
+	    	}
+	    	println(result)
+		}	    
+		
+	}
+}
