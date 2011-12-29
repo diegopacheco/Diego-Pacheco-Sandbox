38f5955fbebb818ceb7ae93ff3967ec6f3e5d9ba
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-12-28 23:49:31 -0200

    playing with traits and generics in scala

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/basic/oo/traits/GenericTrait.scala
@@ -0,0 +1,18 @@
+package com.github.diegopacheco.sandbox.scripts.scala.basic.oo.traits
+
+trait Printable{
+   type Z
+   def print(v:Z):Unit
+}
+
+class ScalaPrinter extends Printable{
+   type Z = String
+   def print(s:String):Unit = println(s)
+}
+
+object GenericTrait {
+	def main(args: Array[String]) {
+		val printer = new ScalaPrinter
+		printer.print("Test String")
+	}
+}
---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/basic/oo/traits/GenericTrait2.scala
@@ -0,0 +1,16 @@
+package com.github.diegopacheco.sandbox.scripts.scala.basic.oo.traits
+
+trait Outable[A]{
+   def print(v:A):Unit
+}
+
+class ScalaOuter[A] extends Outable[A]{
+   def print(s:A):Unit = println(s)
+}
+
+object GenericTrait2 {
+	def main(args: Array[String]) {
+		val printer = new ScalaOuter[String]
+		printer.print("Test String")
+	}
+}
