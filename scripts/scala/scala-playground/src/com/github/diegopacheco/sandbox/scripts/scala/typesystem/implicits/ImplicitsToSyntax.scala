6249bcac020197bbe3756bed9dc8d20eb163b76b
Diego Pacheco <diego.pacheco.it@gmail.com>
2012-01-23 03:52:05 -0200

    added scala implicits toSyntax

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/typesystem/implicits/ImplicitsToSyntax.scala
@@ -0,0 +1,24 @@
+package com.github.diegopacheco.sandbox.scripts.scala.typesystem.implicits
+
+// Source: http://stackoverflow.com/questions/1538598/how-in-scala-to-find-unique-items-in-list
+object ListExtra{
+	def unique[A](ls: List[A]):List[A] = {
+	    def loop(set: Set[A], ls: List[A]): List[A] = ls match {
+	  		case hd :: tail if set contains hd => loop(set, tail)
+	  		case hd :: tail => hd :: loop(set + hd, tail)
+	  		case Nil => Nil
+	    }	
+	    loop(Set(), ls)
+   }	
+}
+
+object ListExtraImplicits{
+	implicit def listToSyntax[A](ls: List[A]) = new {
+		def unique = ListExtra.unique(ls)
+	}
+}
+
+object ImplicitsToSyntaxMain extends App {
+	import ListExtraImplicits._
+	println( List(1, 1, 2, 3, 4, 5, 4).unique  )		
+}
