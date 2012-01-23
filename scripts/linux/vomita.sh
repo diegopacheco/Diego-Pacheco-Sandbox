f762ace88185a7f9ac0b2afbbab624470ab83852
Diego Pacheco <diego.pacheco.it@gmail.com>
2012-01-23 01:42:55 -0200

    update the GPBC script

---
+++ scripts/linux/vomita.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add . 
+git commit -am "$1"
+git push origin master
--- scripts/windows/list-scripts.bat
+++ scripts/windows/list-scripts.bat
@@ -1,1 +1,4 @@
 ls "/cygdrive/c/Users/Diego Pacheco/Diego-Pacheco-Sandbox/scripts/windows" -lsa --color
+
+ls "/cygdrive/c/Users/Diego Pacheco/Diego-Pacheco-Sandbox/scripts/linux" -lsa --color
+
