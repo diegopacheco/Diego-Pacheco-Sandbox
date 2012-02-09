6b69bc31f0d763122615b51b088fc55a5fc607d9
diego.pacheco <diego.pacheco.it@gmail.com>
2012-02-09 18:57:14 -0200

    change script to look for annotations

--- scripts/ruby/top_momber.rb
+++ scripts/ruby/top_momber.rb
@@ -1,7 +1,10 @@
 require 'find'
 
 class JavaMonster		
+	
 	@@pattern  = /.+\.java$/
+	@@total_refactorings = 0
+	@@total_doublecheck  = 0
 	
     class MonsterFile	
     	  attr_accessor :name, :size	
@@ -14,27 +17,85 @@ class JavaMonster
     def top3(path, silence = false)	
 		clear_score
   	    Find.find(path) do |entry|
-    			if File.file?(entry) and entry[@@pattern]
-                    name  = extract(entry)			
-    				lines  = search(entry,name,silence)
-    				score(lines,name)								
-    			end
-  		end
-		printMonsters(path)
+			if File.file?(entry) and entry[@@pattern] and should_scan?(path,entry)
+			    scan(path,entry,silence)
+			end	    			
+  		end				
 	end	
 	
+	def post_proccess(path,t)
+		printMonsters(path)
+		print_annotations_count
+		puts "This script run in #{t}"
+	end
+	
 	private
+	
+	def scan(path,entry,silence)		
+		name   = extract(entry)			
+		lines  = search(entry,name,silence)
+		score(lines,name)
+	end
+	
+	def should_scan?(path,entry)
+		if (entry.include? ".svn" or 
+		   entry.include? ".gitignore" or
+		   entry.include? "ngs-ontology" or 
+		   entry.include? "ngs-autocomplete" or
+		   entry.include? "no-jndi" or
+		   entry.include? "/conf/" or
+		   entry.include? "database" or
+		   entry.include? "/.settings" or 
+		   entry.include? "/target/")
+		   return false
+		else
+           return true
+		end	
+	end
+	
+	def print_annotations_count
+	   puts "@Refactoring => #{@@total_refactorings}"
+	   puts "@DoubleCheck => #{@@total_doublecheck}"
+	end
 	
 	def extract(entry)			
 		File.basename(entry)
 	end
 	
 	def search(entry,name,silence)
+	    javaClass = ""
 		lines = 0
+		
+		#puts "scanning name: #{name} - entry: #{entry} ;;; "
 		f = File.open(entry, "r")		
-		f.each_line { lines = lines + 1 }
-		puts "#{lines.to_s.ljust(6)} - #{name}" unless silence
+		f.each_line { |l|
+			lines     = lines + 1 
+			javaClass = javaClass + l
+		}
+		
+		debug_if_needed(silence)
+		count_refactoring(name,javaClass)
+		count_doublecheck(name,javaClass)
+		
 		lines
+	end
+	
+	def debug_if_needed(silence)
+		puts "#{lines.to_s.ljust(6)} - #{name}" unless silence	
+	end
+	
+	def count_refactoring(name,javaClass)
+		count = javaClass.scan("@Refactoring").size.to_i
+		if count >= 1 
+			@@total_refactorings = @@total_refactorings + count
+		end	
+	end
+	
+	def count_doublecheck(name,javaClass)
+		count = javaClass.scan("@DoubleCheck").size.to_i
+		if count >= 1 
+			@@total_doublecheck = @@total_doublecheck + count
+		end	
 	end
 	
 	def score(lines,name)
@@ -59,16 +120,19 @@ class JavaMonster
         end 		   
 	end
 	
-	def printMonsters(path)		
-		puts " =============================================================== "
-		puts " =============== TOP MONSTERS ================================== "
-		puts " =============================================================== "
-		puts " Project: #{path}"
-		puts " =============================================================== "
-		puts " 1 [Bieber     ] is #{@@hash_top[:first].size.to_s.ljust(6)} LOC  - #{@@hash_top[:first].name}"
-		puts " 2 [Kutcher    ] is #{@@hash_top[:second].size.to_s.ljust(6)} LOC  - #{@@hash_top[:second].name}"
-		puts " 3 [Timberlake ] is #{@@hash_top[:third].size.to_s.ljust(6)} LOC  - #{@@hash_top[:third].name}"
-		puts " =============================================================== "
+	def printMonsters(path)			
+		size = @@hash_top[:first].size + @@hash_top[:second].size + @@hash_top[:third].size
+		if (size>=1)
+			puts " =============================================================== "
+			puts " =============== TOP MONSTERS ================================== "
+			puts " =============================================================== "
+			puts " Project: #{path}"
+			puts " =============================================================== "
+			puts " 1 [Bieber     ] is #{@@hash_top[:first].size.to_s.ljust(6)} LOC  - #{@@hash_top[:first].name}"
+			puts " 2 [Kutcher    ] is #{@@hash_top[:second].size.to_s.ljust(6)} LOC  - #{@@hash_top[:second].name}"
+			puts " 3 [Timberlake ] is #{@@hash_top[:third].size.to_s.ljust(6)} LOC  - #{@@hash_top[:third].name}"
+			puts " =============================================================== "
+		end		
 	end
 	
 	def clear_score
@@ -81,14 +145,17 @@ class JavaMonster
 	
 end
 
-start = Time.now
+start   = Time.now
 monster = JavaMonster.new
-#path    = "D:/Diego/svn-repos/wcsiLegra/NextGenTR/Services-SRC"
-#path    = "D:/Diego/svn-repos/wcsiLegra/NextGenTR/Architecture-SRC"
-path     =  "D:/Diego/svn-repos/wcGitHub/Portal-cliente-ilegra/services"
+path    = "D:/diego/svn-repos/wcsiLegra/cortellis/trunk/Services-SRC/services"
+#path   = "D:/diego/svn-repos/wcsiLegra/cortellis/trunk/Architecture-SRC"
+
 Dir.foreach(path) { |d|
    if !File.file?(d)
-	  monster.top3 "#{path}/#{d}", true
+      if (d != "." and d != "..")
+		 monster.top3 "#{path}/#{d}", true
+	  end	 
    end 	
 }
-puts "This script run in #{Time.now - start}"
+monster.post_proccess(path,(Time.now - start))
+
