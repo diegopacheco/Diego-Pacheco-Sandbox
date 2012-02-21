97b9dd059c38b26b68f63dd5df8d3e276f3fb3e8
diego.pacheco <diego.pacheco.it@gmail.com>
2012-02-21 16:47:28 -0200

    change some scripts

--- scripts/windows/list-scripts.bat
+++ scripts/windows/list-scripts.bat
@@ -1,4 +1,5 @@
-ls "/cygdrive/D/diego/github/diego.pacheco/Diego-Pacheco-Sandbox/scripts/windows" -lsa --color
+ls "/cygdrive/D/diego/github/diego.pacheco/Diego-Pacheco-Sandbox/scripts/windows" --color
 
-ls "/cygdrive/D/diego/github/diego.pacheco/Diego-Pacheco-Sandbox/scripts/linux" -lsa --color
+ls "/cygdrive/D/diego/github/diego.pacheco/Diego-Pacheco-Sandbox/scripts/linux" --color
 
+ls "/cygdrive/D/diego/win-scripts" --color
