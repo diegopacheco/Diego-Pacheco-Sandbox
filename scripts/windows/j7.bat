00d2f7ad1bbd316b70990ba418b69d5643198cdd
diego.pacheco <diego.pacheco.it@gmail.com>
2012-03-04 15:29:34 -0300

    Update java 6 and 7 versions. Added basic IO haskell code

--- scripts/windows/j7.bat
+++ scripts/windows/j7.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_02\
+set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_03\
 set JAVA_HOME_BIN=%JAVA_HOME%\bin
 set PATH=%JAVA_HOME_BIN%;%PATH%
 
-echo using JDK 7_u02 as JAVA_HOME at %DATE% %TIME%
+echo using JDK 7_u03 as JAVA_HOME at %DATE% %TIME%
