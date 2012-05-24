4aeb9e75260eb109b7abe7f4ce91a481758c9bb6
diego.pacheco <diego.pacheco.it@gmail.com>
2012-05-24 16:51:19 -0300

    added more scripts
    added more scripts for deploy

--- scripts/windows/deploy-arch.bat
+++ scripts/windows/deploy-arch.bat
@@ -1,9 +1,11 @@
 cls
 
-SET ngmversion=3.0.0
+SET ngmversion=3.0.2
 SET base_arch=D:\diego\svn-repos\wcsiLegra\cortellis-architecture\Architecture-SRC\
 SET base_jboss=D:\diego\bin\jboss-5.1.0.GA-jdk6\server\default
 SET jboss_deploy=%base_jboss%\deploy
+SET jboss_libs_dir=%base_jboss%\lib
+SET runtime_jboss_dir=%base_arch%\ngm-runtime-jboss
 
 echo Clean up directories: data, log, tmp and work...
 
@@ -18,8 +20,12 @@ copy %base_arch%\ngm-core-cache-jmx\target\ngm-core-cache-jmx-%ngmversion%.jar %
 copy %base_arch%\ngm-ztest-mr-ear\target\ngm-ztest-mr-ear-%ngmversion%.ear %jboss_deploy%\ngm-ztest-mr-ear-%ngmversion%.ear
 copy %base_arch%\ngm-ztest-drug-si-wk-ear\target\ngm-ztest-drug-si-wk-ear-%ngmversion%.ear %jboss_deploy%\ngm-ztest-drug-si-wk-ear-%ngmversion%.ear
 
+pushd %runtime_jboss_dir%
+mvn clean install -P barbarian.arch
+
 echo OFF
 cls
 echo Deploy Time: %DATE%-%TIME% :: to deploy again run: deploy-arch.bat
+echo Libs in [%jboss_libs_dir%] update DONE. 
 echo Deploy {MR, WAR, JMX} DONE - Using NGM [%ngmversion%] on [%base_jboss%]
 
