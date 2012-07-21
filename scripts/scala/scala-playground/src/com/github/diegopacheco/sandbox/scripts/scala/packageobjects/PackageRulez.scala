ffdfe403d1bfa476130f5bb1865c75b1147355ee
diego.pacheco <diego.pacheco.it@gmail.com>
2012-07-21 20:49:30 -0300

    fun with package object in scala
    fun with package object in scala

---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/packageobjects/PackageRulez.scala
@@ -0,0 +1,7 @@
+package com.github.diegopacheco.sandbox.scripts.scala.packageobjects {
+	
+    object Printer {
+		def sysout(m:String):Unit = System.out.println(m)
+	}
+    
+}
---
+++ scripts/scala/scala-playground/src/com/github/diegopacheco/sandbox/scripts/scala/packageobjects/TestPackageObject.scala
@@ -0,0 +1,5 @@
+package com.github.diegopacheco.sandbox.scripts.scala.packageobjects
+
+object TestPackageObject extends App {
+	Printer.sysout("Yeah !!! ")
+}
