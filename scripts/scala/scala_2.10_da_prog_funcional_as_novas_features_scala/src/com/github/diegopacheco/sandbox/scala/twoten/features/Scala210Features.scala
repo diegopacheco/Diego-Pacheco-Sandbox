12cfdb32bda5a6b3deba6ec965c795f6fa5b86da
diego.pacheco <diego.pacheco.it@gmail.com>
2013-04-02 01:09:52 -0300

    added more code for the presentation
    added more code for the presentation

--- scripts/scala/scala_2.10_da_prog_funcional_as_novas_features_scala/src/com/github/diegopacheco/sandbox/scala/twoten/features/Scala210Features.scala
+++ scripts/scala/scala_2.10_da_prog_funcional_as_novas_features_scala/src/com/github/diegopacheco/sandbox/scala/twoten/features/Scala210Features.scala
@@ -1,7 +1,31 @@
 package com.github.diegopacheco.sandbox.scala.twoten.features
 
+// Value Class
+
 object Scala210Features extends App {
 	
-	
-  
+    class RichInt(val value: Int) extends AnyVal {
+    	def toUpperHexString: String = java.lang.Integer.toHexString(value).toUpperCase
+    }
+    
+    val x = new RichInt(2) // at run time, x is simply an integer!
+    println( x.toUpperHexString )
+    
 }
+
+// Better Implicits
+
+trait AwesomeStringImplicits {      	
+  
+  class AwesomeString(s:String) {
+	    def awesome_! = s + "! Awesome!"
+  }
+  
+  implicit def asAwesomeString(s: String) = new AwesomeString(s)
+}
+
+object Main extends App with AwesomeStringImplicits {  
+  println( "Scala".awesome_! )
+}
+ 
+
