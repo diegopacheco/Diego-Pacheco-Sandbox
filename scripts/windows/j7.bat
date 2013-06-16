0d2c6c40c1eb0c3a1c695cc51d7eb63271d87055
diego.pacheco <diego.pacheco.it@gmail.com>
2013-06-16 00:35:33 -0300

    update to java 7 u 17
    update to java 7 u 17

--- scripts/windows/j7.bat
+++ scripts/windows/j7.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_03\
+set JAVA_HOME=C:\Progra~1\Java\jdk1.7.0_17
 set JAVA_HOME_BIN=%JAVA_HOME%\bin
 set PATH=%JAVA_HOME_BIN%;%PATH%
 
-echo using JDK 7_u03 as JAVA_HOME at %DATE% %TIME%
+echo using JDK 7_u17 as JAVA_HOME at %DATE% %TIME%
