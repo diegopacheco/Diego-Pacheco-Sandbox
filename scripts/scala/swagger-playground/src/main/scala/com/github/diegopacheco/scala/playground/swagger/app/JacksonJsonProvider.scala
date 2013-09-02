8d242ca60e16083d109316e03c724834fd291dbc
Diego Pacheco <diego.pacheco.it@gmail.com>
2013-09-02 19:48:10 -0300

    try fix scala constructors issues
    try fix scala constructors issues

--- scripts/scala/swagger-playground/src/main/scala/com/github/diegopacheco/scala/playground/swagger/app/JacksonJsonProvider.scala
+++ scripts/scala/swagger-playground/src/main/scala/com/github/diegopacheco/scala/playground/swagger/app/JacksonJsonProvider.scala
@@ -13,17 +13,20 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider
 @Provider
 @Produces(Array(MediaType.APPLICATION_JSON))
 class JacksonJsonProvider(var commonMapper:ObjectMapper = null) extends JacksonJaxbJsonProvider {
-
-    if(commonMapper == null){
-        val mapper:ObjectMapper = new ObjectMapper()
-
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        
-        commonMapper = mapper
-    }
-    super.setMapper(commonMapper)
+	
+	def initMapper():ObjectMapper = {
+	    if(commonMapper == null) {
+	        var mapper:ObjectMapper = new ObjectMapper()
+	    	mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+	    	mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT)
+	    	mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+	    	mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+	    	return mapper
+	    } else {
+	        return commonMapper
+	    }
+	}
   
+	def this() = this( commonMapper = initMapper() )
+ 
 }
