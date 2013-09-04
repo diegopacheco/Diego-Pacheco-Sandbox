428992810a682214dc0e7b96073bb74d3b91b8b7
diego.pacheco <diego.pacheco.it@gmail.com>
2013-09-04 18:01:43 -0300

    code on swagsger test
    code on swagsger test

--- scripts/scala/swagger-playground/src/main/scala/com/github/diegopacheco/scala/playground/swagger/app/UserResource.scala
+++ scripts/scala/swagger-playground/src/main/scala/com/github/diegopacheco/scala/playground/swagger/app/UserResource.scala
@@ -1,21 +1,32 @@
 package com.github.diegopacheco.scala.playground.swagger.app
 
-import javax.ws.rs.core.Response
-import javax.ws.rs._
-import com.wordnik.swagger.annotations.ApiParam
 import com.wordnik.swagger.annotations.Api
+import com.wordnik.swagger.annotations.ApiErrors
 import com.wordnik.swagger.annotations.ApiOperation
+import com.wordnik.swagger.annotations.ApiParam
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Response
+import javax.xml.bind.annotation.XmlRootElement
+import com.wordnik.swagger.annotations.ApiError
 
 @Path("/user")
 @Api(value="/user", description = "Operations about user")
-@Produces(Array("application/json"))
+@XmlRootElement(name = "Pet")
+@Produces(Array("application/json","application/xml"))
 class UserResource {
 
-  @POST
-  @ApiOperation(value = "Create user", notes = "This can only be done by the logged in user.")
+  @Path("/create")
+  @GET
+  @ApiOperation(value = "Create User", notes = "More notes about this method")
+  @ApiErrors(value = Array(
+      new ApiError(code = 400, reason = "Invalid ID supplied"),
+      new ApiError(code = 404, reason = "Pet not found") 
+  ))
   def createUser(@ApiParam(value = "Created name of the user", required = true) name:String):Response = {
 	println("Saved with sucess!")  
-    return Response.ok().entity("").build()
+    return Response.ok().entity("OK").build()
   }
   
 }
