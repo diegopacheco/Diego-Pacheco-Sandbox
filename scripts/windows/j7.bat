b5ffafa943a397a4705b6736b34c65333dcdc1c2
Diego Pacheco <diego.pacheco.it@gmail.com>
2014-09-19 11:18:15 -0300

    added
    added

--- scripts/windows/j7.bat
+++ scripts/windows/j7.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_51\
+set JAVA_HOME=C:\PROGRA~1\Java\jdk1.7.0_67\
 set JAVA_HOME_BIN=%JAVA_HOME%\bin
 set PATH=%JAVA_HOME_BIN%;%PATH%
 
-echo using JDK 7_u51 as JAVA_HOME at %DATE% %TIME%
+echo using JDK 7_u67 as JAVA_HOME at %DATE% %TIME%
--- scripts/windows/j8.bat
+++ scripts/windows/j8.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
-set JAVA_HOME=C:\PROGRA~1\Java\jdk1.8.0_05
+set JAVA_HOME=C:\PROGRA~1\Java\jdk1.8.0_20
 set JAVA_HOME_BIN=%JAVA_HOME%\bin
 set PATH=%JAVA_HOME_BIN%;%PATH%
 
-echo using JDK 8_u05 as JAVA_HOME at %DATE% %TIME%
+echo using JDK 8_u20 as JAVA_HOME at %DATE% %TIME%
