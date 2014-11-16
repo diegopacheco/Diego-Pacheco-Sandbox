755c0e2e1e5794dc70ec7e5af0be8b0db8078a92
Diego Pacheco <diego.pacheco.it@gmail.com>
2014-11-16 23:32:02 +0100

    added more rust code
    added more rust code

--- scripts/rust/rust-fun/simple.rs
+++ scripts/rust/rust-fun/simple.rs
@@ -1,6 +1,14 @@
+struct Point {
+    x: int,
+    y: int,
+}
+
 fn main() {
 	let x: int;
 	x = 5i;
-
     println!("Hello, world! x: {}", x);
+
+    let origin = Point { x: 0i, y: 0i };
+    println!("The origin is at ({}, {})", origin.x, origin.y);
+
 }
