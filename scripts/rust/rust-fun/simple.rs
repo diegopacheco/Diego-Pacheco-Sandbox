aae8918a2db574cc9abb3863eb7054140853f66f
Diego Pacheco <diego.pacheco.it@gmail.com>
2014-11-16 23:43:53 +0100

    more code
    more code

--- scripts/rust/rust-fun/simple.rs
+++ scripts/rust/rust-fun/simple.rs
@@ -11,4 +11,25 @@ fn main() {
     let origin = Point { x: 0i, y: 0i };
     println!("The origin is at ({}, {})", origin.x, origin.y);
 
+    let y = 5i;
+	match y {
+	    1 => println!("one"),
+	    2 => println!("two"),
+	    3 => println!("three"),
+	    4 => println!("four"),
+	    5 => println!("five"),
+	    _ => println!("something else"),
+	}
+
+	let input = std::io::stdin().read_line().ok().expect("Failed to read line");
+    println!("You inputed: {}", input);
+
+    hello::print_hello();
+    
+}
+
+mod hello {
+    pub fn print_hello() {
+        println!("Hello, world!");
+    }
 }
