64671b7b8bc030f672319255fb60295cafc9f383
Diego Pacheco <diego.pacheco.it@gmail.com>
2014-11-28 15:39:23 -0200

    kick ass
    kick ass

--- scripts/scala/scala-partial-function/PartialFunctionMain.scala
+++ scripts/scala/scala-partial-function/PartialFunctionMain.scala
@@ -1,13 +1,15 @@
 object PartialFunctionMain extends App {
 
 	val sample = 1 to 10
-	
+
 	val isEven: PartialFunction[Int, String] = {
 	  case x if x % 2 == 0 => x+" is even"
 	}
 
 	// the method collect can use isDefinedAt to select which members to collect
 	val evenNumbers = sample collect isEven
+
+	println(s"Even Numbers: ${evenNumbers} ")
 
 	val isOdd: PartialFunction[Int, String] = {
 	  case x if x % 2 == 1 => x+" is odd"
@@ -17,6 +19,6 @@ object PartialFunctionMain extends App {
 	// input outside the declared domain
 	val numbers = sample map (isEven orElse isOdd)
 
-	println(numbers)
+	println(s"Odd or Even Numbers: ${numbers} ")
 
 }	
