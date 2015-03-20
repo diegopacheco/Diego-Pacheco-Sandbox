d9bf168b880651de8591088462d139eedb0e93cb
diegopacheco <diego.pacheco.it@gmail.com>
2015-03-19 21:13:15 -0300

    .
    .

--- scripts/java/guice-gdrl-fun/src/main/java/com/github/diegopacheco/sandbox/java/gdrl/guice/module/BillingModule.java
+++ scripts/java/guice-gdrl-fun/src/main/java/com/github/diegopacheco/sandbox/java/gdrl/guice/module/BillingModule.java
@@ -1,7 +1,10 @@
 package com.github.diegopacheco.sandbox.java.gdrl.guice.module;
 
+import com.github.diegopacheco.sandbox.java.gdrl.guice.Bill;
+import com.github.diegopacheco.sandbox.java.gdrl.guice.BrazillianEletronicBill;
 import com.github.diegopacheco.sandbox.java.gdrl.guice.PayPalPaymentService;
 import com.github.diegopacheco.sandbox.java.gdrl.guice.Payment;
+import com.github.diegopacheco.sandbox.java.gdrl.guice.annotations.bindings.NFE;
 import com.github.diegopacheco.sandbox.java.gdrl.guice.logger.InfoLogger;
 import com.google.inject.AbstractModule;
 
@@ -11,6 +14,7 @@ public class BillingModule extends AbstractModule {
 	  protected void configure() {
 	    bind(Payment.class).to(PayPalPaymentService.class);
 	    bind(InfoLogger.class);
+	    bind(Bill.class).annotatedWith(NFE.class).to(BrazillianEletronicBill.class);
 	  }
 	
 }
