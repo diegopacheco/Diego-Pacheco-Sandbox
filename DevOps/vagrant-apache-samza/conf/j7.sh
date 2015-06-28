a8e08ba2c80b7550bcafd140fb2cca066b8641f9
diegopacheco <diego.pacheco.it@gmail.com>
2015-06-28 18:14:40 -0300

    .
    .

---
+++ DevOps/vagrant-apache-samza/conf/j7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/
+export JRE_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre
+#export PATH=$PATH:/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin:/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/bin
+
---
+++ DevOps/vagrant-apache-spark/conf/j8.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/opt/jdk1.8.0_45
+export JRE_HOME=/opt/jdk1.8.0_45/jre
+#export PATH=$PATH:/opt/jdk1.8.0_45/bin:/opt/jdk1.8.0_45/jre/bin
+
