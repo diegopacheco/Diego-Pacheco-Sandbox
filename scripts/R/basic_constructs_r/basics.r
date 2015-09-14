bb37d4e80ecab796d0410d025fde1e6ae3373936
diegopacheco <diego.pacheco.it@gmail.com>
2015-09-13 21:28:09 -0700

    .
    .

---
+++ scripts/R/basic_constructs_r/basics.r
@@ -0,0 +1,24 @@
+#
+# Vars
+#
+i = 10
+f = 10.6
+s = "Diego String"
+b = TRUE
+
+#
+# Sequence
+#
+s1 = 1:10
+
+#
+# Vector
+#
+v1 = c(1,2,3,6)
+
+#
+# Functions
+#
+inc = function(v){ v + 1 }
+inc(5)
+
