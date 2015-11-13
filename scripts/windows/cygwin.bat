5bba4b9c13e9d1fba27ed6fcd29de9c657640884
diegopacheco <diego.pacheco.it@gmail.com>
2015-11-13 11:45:29 -0800

    .
    .

--- scripts/windows/cygwin.bat
+++ scripts/windows/cygwin.bat
@@ -1,1 +1,5 @@
-call D:\diego\bin\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico -
+@ECHO OFF
+
+REM call D:\diego\bin\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico -
+
+D:\diego\bin\cygwin64\bin\bash --login -i
