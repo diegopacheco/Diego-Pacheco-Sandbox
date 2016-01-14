b3549d28ccc39699e6063b028940a74f5632561f
diegopacheco <diego.pacheco.it@gmail.com>
2016-01-14 00:59:24 -0200

    ..
    .

--- DevOps/docker-dynomite/start.sh
+++ DevOps/docker-dynomite/start.sh
@@ -2,4 +2,6 @@
 
 service redis-server start &
 
-nohup sh -c /dynomite/src/dynomite --conf-file=/dynomite/conf/redis_single.yml -v11 -o dynomite_log.txt
+nohup sh -c /usr/local/Prana/build/distributions/Prana-0.1.0-SNAPSHOT/bin/Prana -c /usr/local/dynomite/dynomite_prana.properties &
+
+nohup sh -c /dynomite/src/dynomite --conf-file=/dynomite/conf/redis_single.yml -v11 -o /dynomite/dynomite_log.txt
