491ae6981c2dcef4f40f13ae9ae4148f250495aa
diegopacheco <diego.pacheco.it@gmail.com>
2016-07-26 18:52:18 -0700

    tests with chai and gulp

---
+++ scripts/js/chai-gulp/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var util = require('gulp-util');
+ 
+gulp.task('test', function () {
+    return gulp.src(['test/**/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', util.log);
+});
+ 
+gulp.task('watch-test', function () {
+    gulp.watch(['views/**', 'public/**', 'app.js', 'framework/**', 'test/**'], ['test']);
+});
+
---
+++ scripts/js/chai-gulp/test/simple-assert-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+process.env.NODE_ENV = 'test';
+ 
+describe('Home page', function () {
+    it('should load the page properly', function()
+        {
+            assert.ok(true);
+        });
+ 
+    it('should navigate to login', function()
+        {
+            assert.equal(2 == 4);
+        });
+ 
+    it('should navigate to sign up');
+    it('should load analytics');
+});
+
