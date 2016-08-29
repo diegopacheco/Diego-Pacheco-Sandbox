404b0f63b3b2b90736194ca9760a4fe29beb8978
diegopacheco <diego.pacheco.it@gmail.com>
2016-08-28 22:42:25 -0700

    america poc working

--- scripts/java/jasper/src/main/java/com/github/diegopacheco/sandbox/java/jasper/SimpleReport.java
+++ scripts/java/jasper/src/main/java/com/github/diegopacheco/sandbox/java/jasper/SimpleReport.java
@@ -2,7 +2,6 @@ package com.github.diegopacheco.sandbox.java.jasper;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
