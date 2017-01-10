07c2589caaeb450347bfb44d6645d251bca2d0d8
diegopacheco <diego.pacheco.it@gmail.com>
2017-01-10 13:47:08 -0200

    .

--- serverless/kappa/simple/_src/main.py
+++ serverless/kappa/simple/_src/main.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import logging
+import requests
+from simpleeggs.emath import *
 
 LOG = logging.getLogger()
 LOG.setLevel(logging.DEBUG)
 
-
 def handler(event, context):
     assert context
     LOG.debug(event)
-    return {'status': 'success'}
+    r = requests.get('https://github.com/timeline.json')
+    code = "http = " + str(r.status_code) + " sum =  " + str(sumEggs(2,2))
+    return {'status': 'success - ' + str(code)  }
