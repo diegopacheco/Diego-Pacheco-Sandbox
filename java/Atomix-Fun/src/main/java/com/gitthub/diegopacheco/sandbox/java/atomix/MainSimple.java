62870fe5258e62fb2f33970f50a7652c4edba267
diegopacheco <diego.pacheco.it@gmail.com>
2017-01-19 12:59:39 -0200

    .

---
+++ java/Atomix-Fun/src/main/java/com/gitthub/diegopacheco/sandbox/java/atomix/MainSimple.java
@@ -0,0 +1,37 @@
+package com.gitthub.diegopacheco.sandbox.java.atomix;
+
+import java.util.concurrent.CompletableFuture;
+
+import io.atomix.AtomixReplica;
+import io.atomix.catalyst.transport.Address;
+import io.atomix.collections.DistributedMap;
+import io.atomix.collections.DistributedQueue;
+import io.atomix.variables.DistributedValue;
+
+public class MainSimple {
+	public static void main(String[] args) throws Throwable{
+		
+		AtomixReplica atomixReplica = AtomixReplica.builder(
+					new Address("localhost", 8700))
+				.build()
+				.bootstrap()
+				.get();
+		
+		System.out.println(atomixReplica);
+		
+		DistributedValue<Object> value = atomixReplica.getValue("value").join();
+		value.set("Hello world!").join();
+		
+		value.get().thenAccept(result -> {
+		  System.out.println("The value is " + result);
+		});
+		
+		
+		DistributedMap<String, String> map = atomixReplica.<String,String>getMap("map").join();
+		map.put("bar", "Hello world!").thenRun(() -> {
+		   map.get("bar").join();
+		});
+		System.out.println(map.get("bar"));
+
+	}
+}
