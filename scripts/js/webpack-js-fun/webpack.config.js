d62073ca7130b55612380b54de2bdf7b9a4688f7
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-01 15:20:18 -0200

    added new module

--- scripts/js/webpack-js-fun/webpack.config.js
+++ scripts/js/webpack-js-fun/webpack.config.js
@@ -4,7 +4,8 @@ const config = {
   entry: './src/index.js',
   output: {
      filename: 'bundle.js',
-     path: path.resolve()
+     //path: path.resolve()
+     path: path.resolve(__dirname, 'build')
   }
 }
 
