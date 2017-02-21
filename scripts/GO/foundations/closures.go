f4dc1f0a3ff3974bac2a9cae6dc477e7d80d1544
diegopacheco <diego.pacheco.it@gmail.com>
2017-02-21 01:06:02 -0300

    added linter on code

--- scripts/GO/foundations/closures.go
+++ scripts/GO/foundations/closures.go
@@ -3,21 +3,21 @@ package main
 import "fmt"
 
 func intSeq() func() int {
-    i := 0
-    return func() int {
-        i += 1
-        return i
-    }
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
 
 func main() {
 
-    nextInt := intSeq()
+	nextInt := intSeq()
 
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
 
-    newInts := intSeq()
-    fmt.Println(newInts())
+	newInts := intSeq()
+	fmt.Println(newInts())
 }
