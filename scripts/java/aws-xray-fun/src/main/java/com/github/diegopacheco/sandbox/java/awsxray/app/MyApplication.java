529d4b075d36f79c5222b1e23a211c4fae9cf0a7
diegopacheco <diego.pacheco.it@gmail.com>
2017-05-16 15:13:50 -0300

    app

--- scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
+++ scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
@@ -1,9 +1,13 @@
 package com.github.diegopacheco.sandbox.java.awsxray.app;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 public class MyApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+	public static void main(String[] args) {
+	    new MyApplication().configure(new SpringApplicationBuilder(SpringBootApp.class)).run(args);
+	}
 }
