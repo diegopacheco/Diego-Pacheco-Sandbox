afe5cc68f58cf58c6a116e91231f84ca853b578b
diegopacheco <diego.pacheco.it@gmail.com>
2017-05-16 15:41:30 -0300

    Added axctuator

--- scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
+++ scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
+import com.github.diegopacheco.sandbox.java.awsxray.rest.CalcController;
+
 @SpringBootApplication
 public class MyApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 	public static void main(String[] args) {
-	    new MyApplication().configure(new SpringApplicationBuilder(SpringBootApp.class)).run(args);
+	    new MyApplication().configure(new SpringApplicationBuilder(CalcController.class)).run(args);
 	}
 }
