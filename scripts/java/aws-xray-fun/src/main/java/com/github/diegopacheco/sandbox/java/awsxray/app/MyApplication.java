5d41875421a007ee07b8d71c1be85d97471785d9
diegopacheco <diego.pacheco.it@gmail.com>
2017-05-16 18:43:46 -0300

    make it simple

--- scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
+++ scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
@@ -1,15 +1,13 @@
 package com.github.diegopacheco.sandbox.java.awsxray.app;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
 
 import com.github.diegopacheco.sandbox.java.awsxray.rest.CalcController;
 
 @SpringBootApplication
-public class MyApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class MyApplication {
 	public static void main(String[] args) {
-	    new MyApplication().configure(new SpringApplicationBuilder(CalcController.class)).run(args);
+	    SpringApplication.run(CalcController.class, args);
 	}
 }
