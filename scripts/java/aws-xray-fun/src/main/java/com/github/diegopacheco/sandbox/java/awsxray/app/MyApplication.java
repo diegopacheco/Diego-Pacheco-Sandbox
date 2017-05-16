e5e6db0bf464e51bb7a2d88c4184ea1c00715c58
diegopacheco <diego.pacheco.it@gmail.com>
2017-05-16 13:28:05 -0300

    Simple Boot + Xray

---
+++ scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/app/MyApplication.java
@@ -0,0 +1,9 @@
+package com.github.diegopacheco.sandbox.java.awsxray.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+}
---
+++ scripts/java/aws-xray-fun/src/main/java/com/github/diegopacheco/sandbox/java/awsxray/rest/CalcController.java
@@ -0,0 +1,27 @@
+package com.github.diegopacheco.sandbox.java.awsxray.rest;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class CalcController {
+
+	@RequestMapping("/sum/{a}/{b}")
+	@ResponseBody
+	Double sum(@PathVariable(value="a") Double a,
+			   @PathVariable(value="b") Double b) {
+		return a + b;
+	}
+	
+	@RequestMapping("/sub/{a}/{b}")
+	@ResponseBody
+	Double sub(@PathVariable(value="a") Double a,
+			   @PathVariable(value="b") Double b) {
+		return a - b;
+	}
+
+}
