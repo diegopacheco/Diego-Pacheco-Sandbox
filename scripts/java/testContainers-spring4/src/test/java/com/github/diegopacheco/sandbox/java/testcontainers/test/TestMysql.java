df9a049ae6abe9bc12e7660bdec017bffd19cd39
diegopacheco <diego.pacheco.it@gmail.com>
2017-05-20 14:13:15 -0300

    mysql working

--- scripts/java/testContainers-spring4/src/test/java/com/github/diegopacheco/sandbox/java/testcontainers/test/TestMysql.java
+++ scripts/java/testContainers-spring4/src/test/java/com/github/diegopacheco/sandbox/java/testcontainers/test/TestMysql.java
@@ -7,51 +7,52 @@ import java.sql.Statement;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 
 import com.github.diegopacheco.sandbox.java.testcontainers.AppConfig;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-@SuppressWarnings({"rawtypes","unchecked"})
+@SuppressWarnings({"rawtypes"})
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { AppConfig.class })
 public class TestMysql {
 
-	private static final Logger logger = LoggerFactory.getLogger(TestMysql.class);
-
 	@Test
-	public void testSimple() throws SQLException {
+	public void testSimple() throws Throwable {
 		
-//		GenericContainer mysql =
-//			    new GenericContainer("mysql:latest")
-//			            .withExposedPorts(3306)
-//			               .withEnv("MYSQL_ROOT_PASSWORD", "root")
-//			               .withEnv("MYSQL_USER", "dockUser")
-//			               .withEnv("MYSQL_PASSWORD", "dockPassword")
-//			               .withEnv("MYSQL_DATABASE", "dockDatabase");
-//		
-//		mysql.start();
+		MySQLContainer mysql = new MySQLContainer();
+		mysql.start();
+		
 		try {
-			//ResultSet resultSet = performQuery(mysql.getContainerIpAddress(),"SELECT 1");
-			ResultSet resultSet = performQuery("localhost","SELECT 1");
-			int resultSetInt = resultSet.getInt(1);
-			Assert.assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+			tryExecute(mysql);
 		} finally {
-//			mysql.stop();
+			mysql.stop();
 		}
 	}
 	
-    protected ResultSet performQuery(String ip,String sql) throws SQLException {
+	private void tryExecute(MySQLContainer mysql) {
+		try{
+			
+			ResultSet resultSet = performQuery(mysql,"SELECT 1");
+			int resultSetInt = resultSet.getInt(1);
+			Assert.assertEquals("A basic SELECT query succeeds", 1, resultSetInt);
+			
+			System.out.println("It works: " + resultSetInt);
+			System.out.println("DONE");
+		}catch(Exception e){
+			System.out.println("Error " + e);
+			Assert.fail(e.toString());
+		}
+	}
+	
+    protected ResultSet performQuery(MySQLContainer mysql,String sql) throws SQLException {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:mysql://" + ip + ":3306/dockDatabase");
-        hikariConfig.setUsername("dockUser");
-        hikariConfig.setPassword("dockDatabase");
+        hikariConfig.setJdbcUrl(mysql.getJdbcUrl());
+        hikariConfig.setUsername(mysql.getUsername());
+        hikariConfig.setPassword(mysql.getPassword());
 
         HikariDataSource ds = new HikariDataSource(hikariConfig);
         Statement statement = ds.getConnection().createStatement();
