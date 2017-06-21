a96f99ff905432e6ddde2e0b0acf2483dc6fe7eb
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-06-20 23:25:55 -0300

    Kotlin gradle

---
+++ scripts/kotlin/hello/src/main/kotlin/helloWorld.kt
@@ -0,0 +1,11 @@
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return words.joinToString(separator = " ")
+}
+
+fun main(args: Array<String>) {
+    println(getGreeting())
+}
---
+++ scripts/kotlin/hello/src/test/kotlin/tests.kt
@@ -0,0 +1,8 @@
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TestSource {
+    @Test fun f() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
