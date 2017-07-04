6edb5b0a700304769cac31429a78350985b6a092
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-03 21:59:49 -0300

    Make project work fin with eclipse

--- scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/basic/Function.kt
+++ scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/basic/Function.kt
@@ -1,13 +1,11 @@
 package com.github.diegopacheco.sandbox.kotlin.basic
 
 class Function {
-
 	fun sum(a: Int, b: Int): Int {
 		return a + b
 	}
-	
-	fun main(args: Array<String>) {
-		println(sum(1,2))
-	}
+}
 
+fun main(args: Array<String>) {
+	println(Function().sum(1,2))
 }
