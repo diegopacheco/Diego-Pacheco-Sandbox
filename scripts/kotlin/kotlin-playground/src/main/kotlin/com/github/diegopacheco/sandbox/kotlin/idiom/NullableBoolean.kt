5110cbe7d0ec674f9f612c1aad823b281ef410f4
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-03 23:16:41 -0300

    Added NullableBooolean

---
+++ scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/idiom/NullableBoolean.kt
@@ -0,0 +1,14 @@
+package com.github.diegopacheco.sandbox.kotlin.idiom
+
+fun main(args: Array<String>) {
+	
+	var b:Boolean? = null
+	if (b == true) {
+		println("Fine ${b}")
+	} else {
+		println("Ops ${b}")
+	}
+	
+	println(b)
+	
+}
