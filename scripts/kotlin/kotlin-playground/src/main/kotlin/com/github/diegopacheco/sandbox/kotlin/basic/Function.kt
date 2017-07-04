82e368aaa4f7e45f7aa94fa2da9898fdabdeba43
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-03 22:05:20 -0300

    More or functions and variables

--- scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/basic/Function.kt
+++ scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/basic/Function.kt
@@ -1,11 +1,26 @@
 package com.github.diegopacheco.sandbox.kotlin.basic
 
 class Function {
+
 	fun sum(a: Int, b: Int): Int {
 		return a + b
 	}
+
+	fun sub(a: Int, b: Int) = a - b
+
+	fun printSum(a: Int, b: Int): Unit {
+		println("sum of $a and $b is ${a + b}")
+	}
+
+	fun printSub(a: Int, b: Int) {
+		println("Sub of $a and $b is ${a - b}")
+	}
+
 }
 
 fun main(args: Array<String>) {
-	println(Function().sum(1,2))
+	println(Function().sum(1, 2))
+	println(Function().sub(3, 2))
+	Function().printSum(2, 2)
+	Function().printSub(2, 2)
 }
---
+++ scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/basic/Variables.kt
@@ -0,0 +1,16 @@
+package com.github.diegopacheco.sandbox.kotlin.basic
+
+fun main(args: Array<String>) {
+	
+	val a = 10
+	
+	val b:Int = 20
+	
+	val c:Int
+	c = 30
+	
+	println(a)
+	println(b)
+	println(c)
+	
+}
