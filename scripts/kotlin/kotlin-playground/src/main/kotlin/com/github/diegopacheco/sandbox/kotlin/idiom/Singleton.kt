b97b1d5f2ace130e450bfeae7231218a5d3d9bea
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-03 23:01:07 -0300

    Added Idiom with Singleton

---
+++ scripts/kotlin/kotlin-playground/src/main/kotlin/com/github/diegopacheco/sandbox/kotlin/idiom/Singleton.kt
@@ -0,0 +1,14 @@
+package com.github.diegopacheco.sandbox.kotlin.idiom
+
+object Resource {
+    val name = "Name" 
+}
+
+fun main(args: Array<String>) {
+	
+	println(Resource)
+	println(Resource)
+	println(Resource.name)
+	println(Resource.name)
+	
+}
