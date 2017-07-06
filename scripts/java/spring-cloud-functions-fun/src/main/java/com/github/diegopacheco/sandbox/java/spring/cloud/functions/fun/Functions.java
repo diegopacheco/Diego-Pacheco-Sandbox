18662859cd77886404fa9a6276fa22a566f426c2
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-06 01:01:50 -0300

    First Spring Cloud Function Working

--- scripts/java/spring-cloud-functions-fun/src/main/java/com/github/diegopacheco/sandbox/java/spring/cloud/functions/fun/Functions.java
+++ scripts/java/spring-cloud-functions-fun/src/main/java/com/github/diegopacheco/sandbox/java/spring/cloud/functions/fun/Functions.java
@@ -4,11 +4,8 @@ import java.util.function.Function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.compiler.FunctionCompiler;
-import org.springframework.cloud.function.compiler.proxy.LambdaCompilingFunction;
 import org.springframework.cloud.function.context.FunctionScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ByteArrayResource;
 
 import reactor.core.publisher.Flux;
 
@@ -17,32 +14,8 @@ import reactor.core.publisher.Flux;
 public class Functions {
 	
 	@Bean
-	public Upper upper() {
-		return new Upper();
-	}
-	
-	@Bean
 	public Function<Flux<String>, Flux<String>> lower() {
 		return flux -> flux.map(value -> value.toLowerCase());
-	}
-	
-	@Bean
-	public <T, R> FunctionCompiler<T, R> compiler() {
-		return new FunctionCompiler<>();
-	}
-	
-	@Bean
-	public Function<String, String> compiledUppercase(FunctionCompiler<String, String> compiler) {
-		String lambda = "s -> s.toUpperCase()";
-		LambdaCompilingFunction<String, String> function = new LambdaCompilingFunction<>(new ByteArrayResource(lambda.getBytes()), compiler);
-		function.setTypeParameterizations("String", "String");
-		return function;
-	}
-
-	@Bean
-	public Function<Flux<String>, Flux<String>> compiledLowercase(FunctionCompiler<Flux<String>, Flux<String>> compiler) {
-		String lambda = "f->f.map(o->o.toString().toLowerCase())";
-		return new LambdaCompilingFunction<>(new ByteArrayResource(lambda.getBytes()), compiler);
 	}
 	
 	public static void main(String[] args) throws Exception {
