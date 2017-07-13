d5a727ab1492be0a471966b1eea2bbdb931f57f4
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-07-13 20:47:45 -0300

    simple seg

--- scripts/java/aws-xray-playground/src/main/java/com/github/diegopacheco/aws/xray/main/MainRunner.java
+++ scripts/java/aws-xray-playground/src/main/java/com/github/diegopacheco/aws/xray/main/MainRunner.java
@@ -1,7 +1,48 @@
 package com.github.diegopacheco.aws.xray.main;
 
+import java.net.URL;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.entities.Segment;
+import com.amazonaws.xray.entities.TraceID;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.strategy.FixedSegmentNamingStrategy;
+import com.amazonaws.xray.strategy.SegmentNamingStrategy;
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
+
 public class MainRunner {
-	public static void main(String[] args) {
+	
+	private static SegmentNamingStrategy segmentNamingStrategy;
+	private static AWSXRayRecorder recorder =  AWSXRay.getGlobalRecorder();
+	
+	static{
+		AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
+		URL ruleFile = MainRunner.class.getResource("/sampling-rules.json");
+		builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+		AWSXRay.setGlobalRecorder(builder.build());
+		segmentNamingStrategy = new FixedSegmentNamingStrategy("aws-xray-poc");
+	}
+	
+	public static void approachA() throws Exception{
 		
+		String segmentName = segmentNamingStrategy.nameForRequest(null);
+		Segment segment = recorder.beginSegment(segmentName, new TraceID(), null);
+		
+		Thread.sleep(2000);
+		
+		if (segment.end()) {
+			 recorder.sendSegment(segment);
+		}
+	}
+	
+	public static void main(String[] args) {
+		try{
+			approachA();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
