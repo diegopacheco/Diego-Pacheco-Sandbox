111e0fbef3a6f77b217297382ad4615fbf1cfc3a
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-08-07 17:44:54 -0300

    .

--- scripts/GO/redis/redis-go-app.go
+++ scripts/GO/redis/redis-go-app.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/go-redis/redis"
 )
 
-func main() {
-
+func handle(w http.ResponseWriter, r *http.Request) {
 	client := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+	client.Incr("kcount")
 
-	client.Set("key", "value1", 0)
-
-	val, err := client.Get("key").Result()
+	val, err := client.Get("kcount").Result()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("key", val)
 
+	fmt.Fprintf(w, string("key count: "+string(val)))
+}
+
+func main() {
+	fmt.Print("Serving at localhost:9090... ")
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":9090", nil)
 }
