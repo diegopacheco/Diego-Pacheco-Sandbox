8d38447a2683d429f5bb8f545833351112d42150
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-08-07 17:58:04 -0300

    make it work

--- scripts/GO/redis/redis-go-app.go
+++ scripts/GO/redis/redis-go-app.go
@@ -8,19 +8,21 @@ import (
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	client := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
+	client := redis.NewClient(&redis.Options{Addr: "redis:6379"})
 	client.Incr("kcount")
 
 	val, err := client.Get("kcount").Result()
 	if err != nil {
 		panic(err)
 	}
+	result := string("key count: " + string(val))
 
-	fmt.Fprintf(w, string("key count: "+string(val)))
+	fmt.Print(result)
+	fmt.Fprintf(w, result)
 }
 
 func main() {
-	fmt.Print("Serving at localhost:9090... ")
+	fmt.Print("Serving at 0.0.0.0:9090... ")
 	http.HandleFunc("/", handle)
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe("0.0.0.0:9090", nil)
 }
