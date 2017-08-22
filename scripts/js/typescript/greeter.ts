d8821b5c4087ac2f66460bcb7f9b237fdfc7a0c5
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-08-22 01:03:40 -0300

    .

---
+++ scripts/js/typescript/greeter.ts
@@ -0,0 +1,11 @@
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+var user = { firstName: "Jane", lastName: "User" };
+document.body.innerHTML = greeter(user);
