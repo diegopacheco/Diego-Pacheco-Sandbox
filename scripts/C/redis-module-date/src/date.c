bb74f2e5d4f068796a066a3421d56954cac510da
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-11-07 12:24:23 -0800

    Working...

--- scripts/C/redis-module-date/src/date.c
+++ scripts/C/redis-module-date/src/date.c
@@ -1,31 +1,32 @@
 #include "redismodule.h"
 
-char* getDate() {
+char * getDate() {
     time_t t = time(NULL);
     struct tm *tm = localtime(&t);
-    //char s[64];
-    char *s = (char *) malloc(sizeof(char) * 64);
+    char s[64];
     strftime(s, sizeof(s), "%c", tm);
-    return s;
+    return strdup(s);
 }
 
 static int diegoDate_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     RedisModule_AutoMemory(ctx);
     RedisModule_ReplicateVerbatim(ctx);
 
-    RedisModule_ReplyWithSimpleString(ctx, "OK");
+    RedisModule_ReplyWithSimpleString(ctx, getDate());
     return REDISMODULE_OK;
 }
 
 int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
     if (RedisModule_Init(ctx, "dp", 1, 1) != REDISMODULE_OK) {
+       printf("\n\r****** Init Fail!\n\r\n\r");
        return REDISMODULE_ERR;
     }
 
-    if (RedisModule_CreateCommand(ctx, "dp.DATE",
-    diegoDate_RedisCommand,"current Date", 1,1,1) != REDISMODULE_OK)
-        return REDISMODULE_ERR;
+    if (RedisModule_CreateCommand(ctx, "dp.DATE",diegoDate_RedisCommand,"", 1,1,1) != REDISMODULE_OK){
+       printf("\n\r****** Command Fail!\n\r\n\r");
+       return REDISMODULE_ERR;
+    }
 
     return REDISMODULE_OK;
 }
