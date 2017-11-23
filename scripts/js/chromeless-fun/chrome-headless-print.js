1f4b78c181e0ebb649c054a90a3bdc42df728cac
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-11-22 21:49:38 -0800

    Chromeless + Docker

--- scripts/js/chromeless-fun/chrome-headless-print.js
+++ scripts/js/chromeless-fun/chrome-headless-print.js
@@ -1,20 +1,31 @@
 //
 // new terminal run: /usr/bin/google-chrome --remote-debugging-port=9222 --disable-gpu --headless
 //
-const { Chromeless } = require('chromeless')
+//
+const { Chromeless }  = require('chromeless')
+
+const fs = require('fs');
+function copyFile(src, dest) {
+  let readStream = fs.createReadStream(src);
+  readStream.once('error', (err) => {
+    console.log(err);
+  });
+  readStream.once('end', () => {
+    console.log('done copying');
+  });
+  readStream.pipe(fs.createWriteStream(dest));
+}
 
 async function run() {
   const chromeless = new Chromeless()
-
   const screenshot = await chromeless
     .goto('https://www.google.com')
     .type('chromeless', 'input[name="q"]')
     .press(13)
     .wait('#resultStats')
     .screenshot()
-
+  copyFile(screenshot, '/' + screenshot);
   console.log(screenshot) // prints local file path or S3 url
-
   await chromeless.end()
 }
 
--- scripts/js/chromeless-fun/run-docker.sh
+++ scripts/js/chromeless-fun/run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run chromeless
+docker run --rm --name chromeless chromeless
