8f97ea75d82387532c67eeb46174cc094d41ef33
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-08-02 16:01:16 -0300

    Added mocks

---
+++ scripts/js/my-dream-app/src/app/shared/sentense.mock.ts
@@ -0,0 +1,6 @@
+const SENTENCES_MOCK = [
+    { english: 'I like to learn Angular' , portuguese: 'Eu gosto de aprender Angular' },
+    { english: 'How are you?' , portuguese: 'Como vai você?' },
+    { english: 'We have to learn typescript' , portuguese: 'Nós precisamos aprender typescript' },
+    { english: 'Is angular better than React' , portuguese: 'Angular é melhor que React' }
+];
---
+++ scripts/js/my-dream-app/src/app/shared/sentense.model.ts
@@ -0,0 +1,10 @@
+export class Sentense {
+    public english: string;
+    public portuguese: string;
+
+    constructor(english: string, sportuguese: string) {
+        this.english = english;
+        this.portuguese = this.portuguese;
+    }
+
+}
