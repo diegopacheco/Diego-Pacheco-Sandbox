be2d47a42b321b5979bcf8ae2759d2d3317af132
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-08-06 23:05:13 -0300

    rxjs simple code with js

--- scripts/js/rxjs-fun/index.js
+++ scripts/js/rxjs-fun/index.js
@@ -1,3 +1,12 @@
 var rxjs = require('rxjs');
+const { range } = require('rxjs');
+const { map, filter } = require('rxjs/operators');
 
+console.log("1...");
 rxjs.of(1, 2, 3).forEach( v => console.log(v) );
+
+console.log("2...");
+range(1, 10).pipe(
+    filter(x => x % 2 === 1),
+    map(x => x + x)
+  ).subscribe(x => console.log(x));
