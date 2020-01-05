90e2c469c14b7fcef883b8e5aa736543e49f5816
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-05 13:43:41 -0300

    my first react component

---
+++ scripts/js/react-component-fun/my-first-react-component/src/components/DPFC/DPFC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class DPFC extends React.Component {
+    render() {
+      return (
+        <div className='app'>
+          <p>Diego Pacheco First React Component!</p>
+        </div>
+      );
+    }
+}
+export default DPFC;
