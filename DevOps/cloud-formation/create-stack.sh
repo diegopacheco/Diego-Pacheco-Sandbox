0269b95e3ae152c09a11603f75a0aab91b431c02
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-01-29 01:01:39 -0300

    .

--- DevOps/cloud-formation/create-stack.sh
+++ DevOps/cloud-formation/create-stack.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name ec2 --template-body file://ec2.yml
+aws cloudformation create-stack --stack-name ec2 --region us-west-2 --template-body file://ec2.yml
+
+# aws cloudformation create-stack --stack-name ec2 --region us-west-2 --template-body https://s3-us-west-2.amazonaws.com/cloudformation-templates-us-west-2/EC2InstanceWithSecurityGroupSample.template --parameters "ParameterKey=KeyName,ParameterValue=name"
