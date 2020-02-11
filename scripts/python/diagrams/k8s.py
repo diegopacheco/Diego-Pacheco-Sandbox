d08a6997a4aab63d941a7d26e707e1bcce01f90e
Diego Pacheco <diego.pacheco.it@gmail.com>
2020-02-10 22:27:29 -0300

    fun with diagrams as code

---
+++ scripts/python/diagrams/k8s.py
@@ -0,0 +1,11 @@
+from diagrams import Diagram
+from diagrams.k8s.clusterconfig import HPA
+from diagrams.k8s.compute import Deployment, Pod, ReplicaSet
+from diagrams.k8s.network import Ingress, Service
+
+with Diagram("Exposed Pod with 3 Replicas", show=False):
+    net = Ingress("domain.com") >> Service("svc")
+    net >> [Pod("pod1"),
+            Pod("pod2"),
+            Pod("pod3")] << ReplicaSet("rs") << Deployment("dp") << HPA("hpa")
+
