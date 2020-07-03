b8a6a7ca530353f0ace9f1a5a8d958f39bf570df
diegopacheco <diego.pacheco.it@gmail.com>
2020-07-03 13:49:03 -0300

    fun with playwright

---
+++ scripts/js/playwright-fun/screenshots.js
@@ -0,0 +1,12 @@
+const playwright = require('playwright');
+
+(async () => {
+  for (const browserType of ['chromium', 'firefox', 'webkit']) {
+    const browser = await playwright[browserType].launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://whatsmyuseragent.org/');
+    await page.screenshot({ path: `example-${browserType}.png` });
+    await browser.close();
+  }
+})();
