3896cc04c02822904264fbc43fe8c398498287b1
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-24 12:39:41 -0300

    fun with lua

--- scripts/lua/basic/src/funcs.lua
+++ scripts/lua/basic/src/funcs.lua
@@ -2,5 +2,12 @@ function sum(a,b)
     result = a + b
     return result
 end
-
 print(sum(2,3))
+
+function sumMul(a,b)
+    sum = a + b
+    mul = a * b
+    return sum,mul
+end
+s,m = sumMul(2,3)
+print("Sum is",s,"Mul is",m)
