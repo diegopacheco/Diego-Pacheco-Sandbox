2d51d43cb80f647894f389d12f4b6f7ffa4fc193
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-24 15:05:03 -0300

    fun with lua

---
+++ scripts/lua/basic/src/Sets.lua
@@ -0,0 +1,40 @@
+Set = {}
+
+function Set.new (t)
+    local set = {}
+    setmetatable(set, Set.mt)
+    for _, l in ipairs(t) do set[l] = true end
+    return set
+end
+
+function Set.union (a,b)
+    local res = Set.new{}
+    for k in pairs(a) do res[k] = true end
+    for k in pairs(b) do res[k] = true end
+    return res
+end
+
+function Set.intersection (a,b)
+    local res = Set.new{}
+    for k in pairs(a) do
+        res[k] = b[k]
+    end
+    return res
+end
+
+function Set.tostring (set)
+    local s = "{"
+    local sep = ""
+    for e in pairs(set) do
+        s = s .. sep .. e
+        sep = ", "
+    end
+    return s .. "}"
+end
+
+function Set.print (s)
+    print(Set.tostring(s))
+end
+
+s = Set.new{10, 20, 30, 50}
+Set.print(s)
