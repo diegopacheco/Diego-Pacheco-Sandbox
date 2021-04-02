d1b786651e562c4d8439d6f2658677a327c68161
diegopacheco <diego.pacheco.it@gmail.com>
2021-04-02 03:02:33 -0300

    simple tic tac toe game

--- scripts/node.js/react-tutorial-fun/src/Game.js
+++ scripts/node.js/react-tutorial-fun/src/Game.js
@@ -4,19 +4,77 @@ import React from 'react';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
     );
   }
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}
+
+class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
