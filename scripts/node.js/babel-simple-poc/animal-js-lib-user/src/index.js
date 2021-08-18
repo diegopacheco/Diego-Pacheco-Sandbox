03301c8c146b4461063d0f28d0180b306587b656
diegopacheco <diego.pacheco.it@gmail.com>
2021-08-18 13:20:47 -0300

    Babel + TS and JS together

--- scripts/node.js/babel-simple-poc/animal-js-lib-user/src/index.js
+++ scripts/node.js/babel-simple-poc/animal-js-lib-user/src/index.js
@@ -1,3 +1,9 @@
 const requireLocal = require('app-root-path').require
+
+// Error: Cannot find module 
+// const AnimalsTSLib = requireLocal('../animal-lib-ts/src');
+
+// Need to import the JS generated not the TS code
 const AnimalsTSLib = requireLocal('../animal-lib-ts/lib');
+
 console.log("JS Lib Calling TS lib FISH == " + AnimalsTSLib.Animals.Fish);
