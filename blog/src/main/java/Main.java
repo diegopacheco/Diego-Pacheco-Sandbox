f8a96e63b6453dde5d96209a330945b1c8611e0b
diegopacheco <diego.pacheco.it@gmail.com>
2022-12-29 22:31:08 -0800

    .

--- blog/src/main/java/Main.java
+++ blog/src/main/java/Main.java
@@ -2,11 +2,14 @@ import java.util.Optional;
 
 public class Main{
   public static void main(String args[]){
-   validate(Optional.empty());
+   validate("VOLLETBALL_12354");
   }
 
-  private static void validate(Optional<String> ticketID){
-    if (!ticketID.get().startsWith("FOOTBALL_")){
+  private static void validate(String ticketID){
+    if (null != ticketID && ticketID.startsWith("FOOTBALL_")){
+      throw new IllegalStateException("Ticket its invalid.");
+    }
+    if (null != ticketID && ticketID.startsWith("VOLLEYBALL_")){
       throw new IllegalStateException("Ticket its invalid.");
     }
   }
