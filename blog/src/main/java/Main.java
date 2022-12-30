bf79a5bc5ea1da945e8fa93cfdec8e3461b552d0
diegopacheco <diego.pacheco.it@gmail.com>
2022-12-29 22:45:21 -0800

    impl biz change

--- blog/src/main/java/Main.java
+++ blog/src/main/java/Main.java
@@ -1,12 +1,14 @@
+import java.util.Optional;
+
 public class Main {
     public static void main(String args[]) {
-        validate("VOLLEYBALL_12354");
-        validate("FOOTBALL_12354");
-        validate("FAKE_12354");
+        validate(Optional.of("VOLLEYBALL_12354"));
+        validate(Optional.of("FOOTBALL_12354"));
+        validate(Optional.of("FAKE_12354"));
     }
 
-    private static void validate(String ticketID) {
-        if (null == ticketID || (!ticketID.startsWith("FOOTBALL_") && !ticketID.startsWith("VOLLEYBALL_"))) {
+    private static void validate(Optional<String> ticketID) {
+        if (null == ticketID || !ticketID.get().startsWith("FOOTBALL_")) {
             throw new IllegalStateException("Ticket [" + ticketID + "] its invalid.");
         }
         //... More validations such as ID exist on DB or cache...
