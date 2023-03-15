97fa8c8d4e66e8f9eb1d4782213181d972b4b472
diegopacheco <diego.pacheco.it@gmail.com>
2023-03-15 00:39:14 -0700

    simple js lib

--- scripts/node.js/prototype-simple/calc.js
+++ scripts/node.js/prototype-simple/calc.js
@@ -11,7 +11,7 @@ Calc.prototype.mul = function(a,b) {
 };
 
 Calc.prototype.div = function(a,b) {
-    return a*b;
+    return a/b;
 };
 
 Calc.prototype.sub = function(a,b) {
--- scripts/node.js/prototype-simple/index.js
+++ scripts/node.js/prototype-simple/index.js
@@ -2,3 +2,6 @@ var Calc = require('./calc.js');
 
 console.log(new Calc);
 console.log("2 + 3 == " + new Calc().sum(2,3) );
+console.log("2 * 3 == " + new Calc().mul(2,3) );
+console.log("3 / 3 == " + new Calc().div(3,3) );
+console.log("3 - 2 == " + new Calc().sub(3,2) );
