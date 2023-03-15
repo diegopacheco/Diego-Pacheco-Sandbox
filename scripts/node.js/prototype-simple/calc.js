9f41ced732a1e113b8426779fa8f8af87b6d8c9e
diegopacheco <diego.pacheco.it@gmail.com>
2023-03-15 02:01:43 -0700

    simple js lib

--- scripts/node.js/prototype-simple/calc.js
+++ scripts/node.js/prototype-simple/calc.js
@@ -1,21 +1,93 @@
-function Calc(){
+const redis = require("redis");
 
+let redisClient;
+
+function Calc(url=""){
+    // this way the connection in lazy, only happen when Calc is initialized
+    let resolvedURL = "";
+    if (""!=url){
+        resolvedURL = 'redis://@' + url;
+    }else{
+        resolvedURL = 'redis://@127.0.0.1:6379';
+    }   
+    redisClient = redis.createClient({
+        url: resolvedURL
+    }); 
+    redisClient.on("error", (error) => console.error(`Error on Redis : ${error}`));
+    redisClient.connect();
 };
 
 Calc.prototype.sum = function(a,b) {
-    return a+b;
+    try{
+       let result = a+b;
+       redisClient.incr('op-sum');
+       return result;
+    }catch(e){
+       redisClient.incr('err-sum');
+       throw e;
+    }
 };
 
 Calc.prototype.mul = function(a,b) {
-    return a*b;
+    try{
+        let result = a*b;
+        redisClient.incr('op-mul');
+        return result;
+     }catch(e){
+        redisClient.incr('err-mul');
+        throw e;
+     }
 };
 
 Calc.prototype.div = function(a,b) {
-    return a/b;
+    try{
+        let result = a/b;
+        if (isNaN(result) || undefined==result){
+            throw new Error("DivisionError: " + a + " by " + b);
+        }
+        redisClient.incr('op-div');
+        return result;
+     }catch(e){
+        redisClient.incr('err-div');
+        throw e;
+     }
 };
 
 Calc.prototype.sub = function(a,b) {
-    return a-b;
+    try{
+        let result = a-b;
+        redisClient.incr('op-sub');
+        return result;
+     }catch(e){
+        redisClient.incr('err-sub');
+        throw e;
+     }
 };
+
+Calc.prototype.disconnect = function() {
+    try{
+        (async () => {
+            await redisClient.quit();
+        })();
+    }catch(e){}
+};
+
+Calc.prototype.observability = function(){
+    (async () => {
+       var result = await redisClient.mGet(["op-mul","op-sub","op-div","op-sum",
+                                            "err-mul","err-sub","err-div","err-sum"]);
+       console.log("Observability Status:")
+       console.table({
+        "OK-Ops-Mul": result[0],
+        "OK-Ops-Sub": result[1],
+        "OK-Ops-Div": result[2],
+        "OK-Ops-Sum": result[3],
+        "ERR-Ops-Mul": result[4],
+        "ERR-Ops-Sub": result[5],
+        "ERR-Ops-Div": result[6],
+        "ERR-Ops-Sum": result[7],
+       });
+    })();
+}
 
 module.exports = Calc;
--- scripts/node.js/prototype-simple/index.js
+++ scripts/node.js/prototype-simple/index.js
@@ -1,7 +1,19 @@
 var Calc = require('./calc.js');
 
-console.log(new Calc);
-console.log("2 + 3 == " + new Calc().sum(2,3) );
-console.log("2 * 3 == " + new Calc().mul(2,3) );
-console.log("3 / 3 == " + new Calc().div(3,3) );
-console.log("3 - 2 == " + new Calc().sub(3,2) );
+const calc = new Calc("localhost:6379");
+//const calc = new Calc();
+
+console.log(calc);
+console.log("2 + 3 == " + calc.sum(2,3) );
+console.log("2 * 3 == " + calc.mul(2,3) );
+console.log("3 / 3 == " + calc.div(3,3) );
+console.log("3 - 2 == " + calc.sub(3,2) );
+
+try{
+    console.log("0 / 0 == " + calc.div(0,0) );
+}catch(e){
+    console.log(""+e)
+}
+
+calc.observability();
+calc.disconnect();
