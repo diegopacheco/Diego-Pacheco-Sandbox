aac47b38c886a38f643c6f7f16f92f4ebf3e8b48
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-18 19:18:07 -0700

    stats script

--- scripts/js/github-stats-fun/get-stats.sh
+++ scripts/js/github-stats-fun/get-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-people=diegopacheco
+people="diegopacheco"
 for p in $people
 do 
   count=$(curl -s "https://github.com/users/$p/contributions" |  grep -B2 last | sed 's/[^0-9]*//g' | sed -r 's/\s+//g' | tr --delete '\n');
