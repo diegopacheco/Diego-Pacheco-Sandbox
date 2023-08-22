8825e4e4b25a3098b4a22d18a68ea08f9a2b53c2
diegopacheco <diego.pacheco.it@gmail.com>
2023-08-21 23:02:08 -0700

    .

---
+++ scripts/node.js/decorator-wrap-fun/index.js
@@ -0,0 +1,21 @@
+import wrap from 'decorator-wrap';
+
+var log = (callback, args, name, type) => {
+  console.log('Starting  ', type, name);
+  var result = callback();
+  console.log('Ended: ', name);
+  return result;
+};
+
+@wrap(log)
+class SuperNiceClass {
+    constructor(){
+        console.log('hello from class');
+    }
+    @wrap(log)
+    bar(a,b) {
+        console.log('hello from bar method');
+    }
+}
+
+new SuperNiceClass().bar(1,2);
